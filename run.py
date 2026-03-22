d80f4a3af768da015c9f07b2aa8a7a4b64cf29d5
manoj-pendem01 <pendem.manojmanu01@gmail.com>
2026-03-22 13:13:23 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,16 +1,15 @@
 """
 FoodWise AI - Application Entry Point
 """
-from app import create_app, db
+from app import create_app
 import os
 
-app = create_app(os.getenv("FLASK_ENV", "development"))
+app = create_app(os.getenv("FLASK_ENV", "production"))
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
+    port = int(os.getenv("PORT", 5000))
     app.run(
         host="0.0.0.0",
-        port=int(os.getenv("PORT", 5000)),
-        debug=os.getenv("FLASK_DEBUG", "True") == "True"
+        port=port,
+        debug=False
     )
